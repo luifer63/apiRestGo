8b56a5cb6d4002f2aa9afbcb05087bf845dc122b
luifer63 <lvix2000@gmail.com>
2023-04-09 08:59:48 -0500

    Limpieza de Cache

--- bd/conexionBD.go
+++ bd/conexionBD.go
@@ -10,7 +10,7 @@ import(
 
 //MongoCN -> es el objeto de conexion a BD
 var MongoCN = ConectarBD()
-var clientOptions = options.Client().ApplyURI("mongodb+srv://luifer63:6X7SsPFJqe0pllYB@cluster0.igqhpqx.mongodb.net/test")
+var clientOptions = options.Client().ApplyURI("mongodb+srv://luifer63:sB6v2FhDgY6CNiNH@cluster0.igqhpqx.mongodb.net/test")
 
 // ConectarBD() -> funcion para conectar a base de datos
 func ConectarBD() *mongo.Client{
